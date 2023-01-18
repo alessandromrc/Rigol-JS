2cd1857ab07b59cdf9d4abed4c3cde6e408cb172
Alessandro <66976091+alessandromrc@users.noreply.github.com>
2023-01-18 22:04:59 +0100

    Update screenshot.js
    Updated the code to the last library version

--- screenshot.js
+++ screenshot.js
@@ -1,19 +1,25 @@
 const { RigolConnector } = require("./rigol_features/rigol");
 
-const instrument_ip = "192.168.178.98";
+const instrument_ip = "192.168.178.56";
 const instrument_port = 5555;
 
+const Rigol = new RigolConnector(instrument_ip, instrument_port);
+
 async function Main() {
-  const Rigol = new RigolConnector(instrument_ip, instrument_port);
+  let ts = Date.now();
+  let date_ob = new Date(ts);
+  let date = date_ob.getDate();
+  let month = date_ob.getMonth() + 1;
+  let year = date_ob.getFullYear();
+  let hours = date_ob.getHours();
+  let minutes = date_ob.getMinutes();
+  let seconds = date_ob.getSeconds();
 
-  await Rigol.features.Screenshot("./screenshots/screenshot")
-  .then(() => {
-    console.log("Screenshot saved");
-    setTimeout(() => { process.exit(0); }, 1000);
-  })
-
-
-
+  await Rigol.features.Screenshot("./screenshots/screenshot_" + hours + "_" + minutes + "_" + seconds + "__" + year + "_" + month + "_" + date)
+    .then(() => {
+      console.log("Screenshot saved");
+      Main();
+    })
 }
 
 Main();
